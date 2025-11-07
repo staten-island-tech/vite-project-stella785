c547caf7c5cedb65b7f2e4f716005b6df29ce322
stella785 <stellac35@nycstudents.net>
2025-11-07 14:01:42 -0500

    ..

---
+++ takeanumber.js
@@ -0,0 +1,14 @@
+function number(last, list) {
+    let students = 23;
+    let take = 0;
+    let serve = 0;
+    list.forEach(element => {
+        if () {
+
+        } else if (take > serve) {
+            console.log(take - serve)
+        } else {
+
+        }
+    });
+}
