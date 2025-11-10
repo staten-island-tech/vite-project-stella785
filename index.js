fece5cf61b5bcf556c5fa002b304fab118f360d4
stella785 <stellac35@nycstudents.net>
2025-11-10 14:00:40 -0500

    ......

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ const books = [
         title: "Assistant to the Villain",
         author: "Hannah Nicole Maehrer",
         genre: ["Romance", "Fantasy"],
-        link: ""
+        link: "https://www.barnesandnoble.com/w/assistant-to-the-villain-hannah-nicole-maehrer/1143222643?ean=9781649375803"
     },
     {
         title: "Apprentice to the Villain",
         author: "Hannah Nicole Maehrer",
         genre: ["Romance", "Fantasy"],
-        link: ""
+        link: "https://www.barnesandnoble.com/w/apprentice-to-the-villain-hannah-nicole-maehrer/1144444055?ean=9781649377609"
     },
     {
         title: "Accomplice to the Villain",
         author: "Hannah Nicole Maehrer",
         genre: ["Romance", "Fantasy"],
-        link: ""
+        link: "https://www.barnesandnoble.com/w/accomplice-to-the-villain-hannah-nicole-maehrer/1146458535?ean=9781649379092"
     },
     {
         title: "If Cats Disappeared from the World",
         author: "Genki Kawamura",
-        genre: "Fiction",
-        link: ""
+        genre: ["Fiction"],
+        link: "https://www.barnesandnoble.com/w/if-cats-disappeared-from-the-world-genki-kawamura/1127834244?ean=9781250257680"
     },
     {
         title: "Lord of the Flies",
         author: "William Golding",
         genre: ["Allegory", "YA"],
-        link: ""
+        link: "https://www.barnesandnoble.com/w/lord-of-the-flies-william-golding/1100154846?ean=9780399501487"
     },
     {
         title: "I Am Rebel",
         author: "Ross Montgomery",
-        genre: "Adventure",
-        link: ""
+        genre: ["Adventure"],
+        link: "https://www.barnesandnoble.com/w/i-am-rebel-ross-montgomery/1144510238?ean=9781536246803"
     },
 ]
